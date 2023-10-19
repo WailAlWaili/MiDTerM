a275078ed00347d2de0075e751e47bd84b175db4
“WAIL” <alwaili@umich.edu>
2023-10-19 16:37:10 -0400

    Modified integer values in quraticsolver.java

--- Desktop/quraticsolver.java
+++ Desktop/quraticsolver.java
@@ -3,11 +3,11 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 1;
-		int b = 3;
-		int c = 2;
+		int a = 5;
+		int b = 5;
+		int c = 5;
 		if (b*b - 4*a*c <0)
-			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" demonstrates no solution!");
+			System.out.println("This Math Question " + a + "x^2 + " + b +"x + " + c +" demonstrates no solution!");
 		else
 			{
 				System.out.println("The solution for these equations " + a + "x^2 + " + b +"x + " + c +" are as follows");
